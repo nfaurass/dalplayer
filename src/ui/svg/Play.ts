159ddd38e215fe33d00d258d3a8592b807f82f4f
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-08-08 16:41:18 +0100

    Add controls SVG icons

---
+++ src/ui/svg/Play.ts
@@ -0,0 +1,10 @@
+export default function PlaySVG(color: string = "#fff") {
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" width="24" height="24"
+         data-icon="PlayStandard" aria-hidden="true" class="svg-icon svg-icon-dalplayerPlay">
+            <path d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z"
+                  fill="${color}">
+            </path>
+        </svg>
+    `;
+}
