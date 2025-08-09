010bd10fe057f7dbdb34d2a0dacb6443761b24c5
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-08-09 18:17:52 +0100

    Add LoadingSpinner ui control

---
+++ src/ui/controls/LoadingSpinner.ts
@@ -0,0 +1,31 @@
+export default function LoadingSpinner() {
+    const LoadingSpinner = document.createElement('div');
+    LoadingSpinner.id = 'LoadingSpinner';
+    Object.assign(LoadingSpinner.style, {
+        width: '70px',
+        height: '70px',
+        border: '6px solid transparent',
+        borderTopColor: 'white',
+        borderRightColor: 'white',
+        borderRadius: '50%',
+        animation: 'DALPlayerSpin 1s linear infinite',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        boxSizing: 'border-box',
+        zIndex: '10',
+    });
+
+    const styleSheet = document.createElement('style');
+    styleSheet.type = 'text/css';
+    styleSheet.innerText = `
+        @keyframes DALPlayerSpin {
+            0% { transform: translate(-50%, -50%) rotate(0deg); }
+            100% { transform: translate(-50%, -50%) rotate(360deg); }
+        }
+    `;
+    document.head.appendChild(styleSheet);
+
+    return LoadingSpinner;
+}
