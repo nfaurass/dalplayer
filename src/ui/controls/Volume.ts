6a1996854a09cab3736032a118c80742a65c3b01
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-08-15 03:20:33 +0100

    Update Volume ui control

--- src/ui/controls/Volume.ts
+++ src/ui/controls/Volume.ts
@@ -1,6 +1,25 @@
-export default function VolumeControl(): HTMLButtonElement {
+export default function VolumeControl(): {
+    volumeIcon: HTMLButtonElement,
+    volumeSlider: HTMLInputElement,
+    volumeContainer: HTMLDivElement
+} {
     const volumeIcon = document.createElement('button');
     volumeIcon.id = "VolumeIcon";
     volumeIcon.className = "DALPlayer-button";
-    return volumeIcon;
+
+    const volumeSlider = document.createElement('input');
+    volumeSlider.type = 'range';
+    volumeSlider.min = '0';
+    volumeSlider.max = '1';
+    volumeSlider.step = '0.01';
+    volumeSlider.value = '0';
+    volumeSlider.className = 'DALPlayer-volume-slider';
+
+    const volumeContainer = document.createElement('div');
+    volumeContainer.className = 'DALPlayer-volume-container';
+
+    volumeContainer.appendChild(volumeIcon);
+    volumeContainer.appendChild(volumeSlider);
+
+    return {volumeIcon, volumeSlider, volumeContainer};
 }
