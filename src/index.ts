3a27ae482fbf989ca0cf313b28ada9edbc7b2e2c
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-08-18 19:15:01 +0100

    Update main exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import {DALPlayer} from './core/DALPlayer';
+import {CaptionsPlugin} from './plugins/CaptionsPlugin';
+import {PictureInPicturePlugin} from './plugins/PictureInPicturePlugin';
+import {PlaybackSpeedPlugin} from './plugins/PlaybackSpeedPlugin';
+import {LoopPlugin} from './plugins/LoopPlugin';
+import {DownloadPlugin} from './plugins/DownloadPlugin';
 
-if (typeof window !== 'undefined') (window as any).DALPlayer = DALPlayer;
+if (typeof window !== 'undefined') {
+    const w = window as any;
+    w.DALPlayer = DALPlayer;
+    (DALPlayer as any).CaptionsPlugin = CaptionsPlugin;
+    (DALPlayer as any).PictureInPicturePlugin = PictureInPicturePlugin;
+    (DALPlayer as any).PlaybackSpeedPlugin = PlaybackSpeedPlugin;
+    (DALPlayer as any).LoopPlugin = LoopPlugin;
+    (DALPlayer as any).DownloadPlugin = DownloadPlugin;
+}
 
+export {DALPlayer, CaptionsPlugin, PictureInPicturePlugin, PlaybackSpeedPlugin, LoopPlugin, DownloadPlugin};
 export default DALPlayer;
