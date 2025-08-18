542416c74a0d5b830161d758a5acb927c7b6c957
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-08-18 19:14:23 +0100

    Update ui svgs

---
+++ src/ui/svg/Captions.ts
@@ -0,0 +1,10 @@
+export default function CaptionsSVG(color: string = "#fff") {
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 25 25" width="24" height="24"
+             data-icon="CaptionsStandard" aria-hidden="true" class="svg-icon svg-icon-dalplayerCaptions">
+            <path fill="${color}" fill-rule="evenodd" clip-rule="evenodd"
+                d="M20.773 2.207h-16a4 4 0 0 0-4 4v12a4 4 0 0 0 4 4h16a4 4 0 0 0 4-4v-12a4 4 0 0 0-4-4zm-16 2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-12a2 2 0 0 0-2-2h-16zm3 9a1 1 0 0 1 1-1h8a1 1 0 1 1 0 2h-8a1 1 0 0 1-1-1zm-2 3a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2zm5 1a1 1 0 0 1 1-1h8a1 1 0 1 1 0 2h-8a1 1 0 0 1-1-1z">
+            </path>
+        </svg>
+    `
+}
